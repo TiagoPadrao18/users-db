4950a64a1126182c7ed0c722f42ef2c501b795b3
Tiago <mindera@Mac-Tiago.local>
2024-01-23 15:36:40 +0000

    Exception with Http Status code done.

--- src/main/java/com/usersDb/usersDb/exception/FindByNameException.java
+++ src/main/java/com/usersDb/usersDb/exception/FindByNameException.java
@@ -1,5 +1,11 @@
 package com.usersDb.usersDb.exception;
 
+import org.aspectj.lang.annotation.DeclareError;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_FOUND)
 public class FindByNameException extends RuntimeException{
     public FindByNameException (){
         super("Invalid Name");
